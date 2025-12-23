7326ec217c73fefba29cae21b56c32b568048ed9
Zeamanuel Mebit <mebitzeamanuel@gmail.com>
2025-12-24 01:02:28 +0300

    left over from prev commit

--- src/pages/Workspace/Workspace.jsx
+++ src/pages/Workspace/Workspace.jsx
@@ -1,10 +1,18 @@
 import Header from "../../common-components/Header/Header"
+import Chat from "./components/Chat"
+import CodePanel from "./components/CodePanel"
+import WorkspaceHeader from "./components/WorkspaceHeader"
 
 export default function Workspace() {
     return (
         <>
             <Header />
-            <h1 className="">Workspace</h1>
+            <WorkspaceHeader />
+
+            <div className="workspace-body h-full grid grid-cols-[1fr_34rem] gap-8">
+                <CodePanel />
+                <Chat />
+            </div>
         </>
     )
 }
